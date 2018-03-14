9c27496c8f16aa9c3c025d98ec53f66ee94cc3f4
bimiriya <poyarzunaspee@gmail.com>
2018-03-14 11:31:03 -0300

    firebase

---
+++ react-trivia/src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Login extends Component {
+  render() {
+    return <div>
+              <button>Google Login</button>
+           </div>;
+  }
+}
+
+Login.propTypes = {};
+
+export default Login;
--- react-trivia/src/index.js
+++ react-trivia/src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyBepVwE4HIfCR2I2qicjlxauaCXkLcAQpk",
+  authDomain: "react-trivia.firebaseapp.com",
+  databaseURL: "https://react-trivia.firebaseio.com",
+  projectId: "react-trivia",
+  storageBucket: "react-trivia.appspot.com",
+  messagingSenderId: "88902727572"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
