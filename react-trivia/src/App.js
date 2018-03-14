3238c1cb7c1cad164a128d5988ea2edf36ffe8ff
bimiriya <poyarzunaspee@gmail.com>
2018-03-14 12:21:14 -0300

    google login + user profile pic

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
+import firebase, { auth, provider } from './firebase.js';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentItem: '',
+      username: '',
+      items: [],
+      user: null // <-- add this line
+    };
+
+    this.login = this.login.bind(this); // <-- add this line
+    this.logout = this.logout.bind(this); // <-- add this line
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+  }
+
+  handleChange(e) {
+    /* ... */
+  }
+  logout() {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null
+      });
+    });
+  }
+  login() {
+    auth.signInWithPopup(provider).then(result => {
+      const user = result.user;
+      this.setState({
+        user
+      });
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        
+      <div className="app">
+        <header>
+          <div className="wrapper">
+            <h1>Fun Food Friends</h1>
+            {this.state.user ? (
+              <button onClick={this.logout}>Logout</button>
+            ) : (
+              <button onClick={this.login}>Log In</button>
+            )}
+          </div>
+        </header>
+        {this.state.user ? (
+          <div>
+            <div className="user-profile">
+              <img src={this.state.user.photoURL} />
+            </div>
+          </div>
+        ) : (
+          <div className="wrapper">
+            <p>
+              You must be logged in to see the potluck list and submit to it.
+            </p>
+          </div>
+        )}
       </div>
     );
   }
