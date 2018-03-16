14c35e7bd54dd657be6a2ae2125091fa8ba24d20
IreeRodriguez <ire208@gmail.com>
2018-03-15 22:09:08 -0300

    cambio de pregunta, saber si es buena o mala

--- react-trivia/src/components/GameQuestions.jsx
+++ react-trivia/src/components/GameQuestions.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Row, Col } from 'react-bootstrap';
+import Right from './answers/Right';
+import Wrong from './answers/Wrong';
 
 class GameQuestions extends Component {
   constructor(category, type, diff) {
@@ -10,7 +12,9 @@ class GameQuestions extends Component {
       type,
       diff,
       questions: '',
-      counter: 0
+      counter: 0,
+      answers:[],
+      isValid: null,
     };
   }
 
@@ -26,25 +30,64 @@ class GameQuestions extends Component {
     fetch(url)
       .then(response => response.json())
       .then(data =>
-        data.results.map(quest => ({
+        data.results.map(quest => {
+          let ans = [quest.correct_answer];
+          quest.incorrect_answers.map(index => {
+            ans.push(index);
+          });
+          let i = ans.length;
+          for (; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = ans[i];
+            ans[i] = ans[j];
+            ans[j] = temp;
+          }
+          this.state.answers.push(ans);
+          return ({
           question: `${quest.question}`,
           right: `${quest.correct_answer}`,
           wrong: `${quest.incorrect_answers}`
-        }))
+        })})
       )
       .then(questions =>
+
         this.setState({
-          questions
+          questions,
         })
-      );
+      )
+      .catch(error => console.log(error));
+    }
+    
+  nextQuestion() {
+    const add = this.state.counter + 1;
+    this.setState({
+      counter: add,
+      isValid: null,
+    })
   }
 
-  handleRightAnswer() {
-    console.log('yaaaa')
+  handleRightAnswer(event) {
+    console.log(event.target.name)
+    if (event.target.name === this.state.questions[this.state.counter].right) {
+      this.setState({
+        isValid: true
+      })
+    } else {
+      this.setState({
+        isValid: 'false'
+      })
+    }
+    console.log(this.state);
+    
   }
 
   render() {
     const { questions } = this.state;
+    const { answers } = this.state;
+    const { counter } = this.state;
+    // console.log(answers[counter].length);
+    const finalAnswers = answers[counter];
+    
     return (
       <div>
         {questions.length > 0 ? (
@@ -58,12 +101,27 @@ class GameQuestions extends Component {
             </Row>
             <Row>
             <Col xs={6} sm={6} md={6} lg={6}>
-              <button onClick={this.handleRightAnswer} className="answer btn">{questions[this.state.counter].right}</button>
+              {  answers.length > 0 && this.state.isValid === null ? 
+                finalAnswers.map(ans => {
+                  if (ans===undefined) {
+                    console.log(ans);                    
+                  } else {
+                    const answ = `${ans}`
+                    return (
+                      <button key={ans} name={ans} onClick={this.handleRightAnswer.bind(this)} className="answer btn">{answ}</button>
+                    )
+                  }
+                  })  
+                  : null
+              }
             </Col>
             </Row>
             <Row>
             <Col xs={12} sm={12} md={6} mdOffset={3} lg={6} lgOffset={3} >
-            <button className="btn next-btn">Next</button>
+              {
+        this.state.isValid ? ( this.state.isValid === true ? <Right onClick={this.nextQuestion.bind(this)}/> : <Wrong onClick={this.nextQuestion.bind(this)}/> ) : null
+              }
+            
             </Col>
             </Row>
           </Grid>
---
+++ react-trivia/src/components/answers/Right.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Right = ({ onClick }) => (
+      <div>
+        <button onClick={onClick} className="btn next-btn">Next</button>
+      </div>
+);
+
+
+export default Right;
